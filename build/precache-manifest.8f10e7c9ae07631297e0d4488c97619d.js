c326f594d854b23bcf7d5c91d0b4e05c7613333c
Muhammed Husrevoglu <muhammed.husrevoglu@prodabilisim.com>
2019-12-23 12:12:30 +0300

    initial

---
+++ build/precache-manifest.8f10e7c9ae07631297e0d4488c97619d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24bb59ec97e7b63a5b5ac2954c119dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a01d427cffcea24572e0",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "65a1d148be274b4410a5",
+    "url": "/static/css/main.5089a3dd.chunk.css"
+  },
+  {
+    "revision": "a01d427cffcea24572e0",
+    "url": "/static/js/2.9cafdfd5.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.9cafdfd5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65a1d148be274b4410a5",
+    "url": "/static/js/main.cb8ec048.chunk.js"
+  },
+  {
+    "revision": "eb3a64062771b28d8e3c",
+    "url": "/static/js/runtime-main.1d213bf0.js"
+  }
+]);
